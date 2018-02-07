f9e3635d11d8e2c4e7f835154d07556cbbbbbc61
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-02-07 10:50:33 +0100

    4th example

---
+++ version4/features/steps/common.py
@@ -0,0 +1,56 @@
+import json
+
+from behave import given, then, when
+from urllib.parse import urljoin
+import requests
+
+
+@given('GitHub is accessible')
+def initial_state(context):
+    context.is_accessible(context)
+
+
+@given('System is running')
+def running_system(context):
+    """Ensure that the system is accessible."""
+    assert is_accessible(context)
+
+
+@when('I access the API endpoint {url}')
+def access_endpoint(context, url):
+    context.response = requests.get(context.api_url + url)
+
+
+@when('I search for user with nick {nick}')
+def search_for_user(context, nick):
+    url = urljoin(urljoin(context.api_url, "users/"), nick)
+    context.response = requests.get(url)
+
+
+@then('I should receive {status:d} status code')
+def check_status_code(context, status):
+    """Check the HTTP status code returned by the REST API."""
+    assert context.response.status_code == status
+
+
+@then('I should receive proper JSON response')
+def check_json_response(context):
+    content_type = context.response.headers.get('content-type')
+    assert content_type.startswith('application/json')
+    context.data = context.response.json()
+
+
+@then('I should find the user with full name {fullname}')
+def check_user_full_name(context, fullname):
+    assert context.data is not None
+    assert 'name' in context.data
+    value = context.data.get('name')
+    assert value == fullname, "{e} != {v}".format(e=fullname, v=value)
+
+
+@then('I should find that the user works for company {company}')
+def check_company(context, company):
+    assert context.data is not None
+    assert 'company' in context.data
+    value = context.data.get('company', '')
+    assert value == company, "{e} != {v}".format(e=company, v=value)
