9b4e7e8899e8ba4c9aed480726773cd3efee6712
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-04-02 20:12:53 +0200

    Testing Python function, 1st example

---
+++ test_python_function1/features/steps/common.py
@@ -0,0 +1,18 @@
+from behave import given, then, when
+from src.adder import add
+
+
+@given('The function {function_name} is callable')
+def initial_state(context, function_name):
+    pass
+
+
+@when('I call function {function} with arguments {x:d} and {y:d}')
+def call_add(context, function, x, y):
+    context.result = add(x, y)
+
+
+@then('I should get {expected:d} as a result')
+def check_integer_result(context, expected):
+    assert context.result == expected, \
+        "Wrong result: {r} != {e}".format(r=context.result, e=expected)
