884cbf9d9c33cd6b89f006bc501ffb3f7b24e0eb
Pavel Tisnovsky <ptisnovs@redhat.com>
2018-04-10 08:32:38 +0200

    Behave example: wrong REST API URL

---
+++ github_test_version1_wrong_url/features/steps/common.py
@@ -0,0 +1,21 @@
+import json
+
+from behave import given, then, when
+from urllib.parse import urljoin
+import requests
+
+
+@given('GitHub is accessible')
+def initial_state(context):
+    assert context.is_accessible(context)
+
+
+@when('I access the API endpoint {url}')
+def access_endpoint(context, url):
+    context.response = requests.get(context.api_url + url)
+
+
+@then('I should receive {status:d} status code')
+def check_status_code(context, status):
+    """Check the HTTP status code returned by the REST API."""
+    assert context.response.status_code == status
